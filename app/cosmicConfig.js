a4a1965ff28fbc39fa75569a4234c34072a0967b
Ethan Dinnen <ethan.dinnen@ocean.org>
2018-01-02 11:23:49 -0800

    Added cosmic config and graphql

---
+++ app/cosmicConfig.js
@@ -0,0 +1,6 @@
+export default {
+  bucket: {
+    slug: proccess.env.COSMIC_SLUG || "acd331c0-e69f-11e7-b266-0bda9cde0587",
+    read_key: proccess.env.COSMIC_READ_KEY || "rxA3MdlaFBv7KoWaKwfWbYGs6Sz7UFAzD7L4asuhxZkaO8eWid",
+  }
+}
